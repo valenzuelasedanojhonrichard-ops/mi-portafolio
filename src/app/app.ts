2fd7282de83d15be0deab487646de3c6d70bf0ee
Richard Valenzuela <valenzuelasedanojhon@gmail.com>
2026-04-07 18:02:47 -0500

    primer commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,21 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Navbar } from './components/navbar/navbar';
+import { Hero } from './components/hero/hero';
+import { About} from './components/about/about';
+import { Portfolio } from './components/portfolio/portfolio';
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    Navbar,
+    Hero,
+    About,
+    Portfolio,
+    Footer
+  ],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('mi-portafolio');
---
+++ src/app/components/portfolio/portfolio.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-portfolio',
+  imports: [],
+  templateUrl: './portfolio.html',
+  styleUrl: './portfolio.scss',
+})
+export class Portfolio {}
